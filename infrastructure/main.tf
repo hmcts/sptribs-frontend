cb2f296f43578fef36ccff2ce37ff5d3d906076e
Serkan Geçici <geciciserkan@gmail.com>
2023-05-19 11:41:47 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,7 +14,7 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
 
 module "sptribs-frontend-web-session-storage" {
   source      = "git@github.com:hmcts/cnp-module-redis?ref=add-redis-version-flag"
-  product     = var.product
+  product = "${var.product}-${var.component}-session-storage"
   location    = var.location
   env         = var.env
   subnetid    = data.azurerm_subnet.core_infra_redis_subnet.id
