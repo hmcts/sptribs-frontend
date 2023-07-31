7e0b96802657a62586d44202a1ae6f5439149b3c
Jake Gowler <jakecgowler@gmail.com>
2023-07-31 14:11:02 +0100

    Landing page

--- src/test/e2e/pages/LandingPage.js
+++ src/test/e2e/pages/LandingPage.js
@@ -5,16 +5,16 @@ const LandingpageDetails = require('../fixtures/content/LandingPage_content');
 
 module.exports = {
 
+  startButton: 'a[role="button"]',
+
   async seeTheLandingPage() {
     console.log('User using the URL= ' + config.baseUrl);
     await I.amOnPage(config.baseUrl)
-    I.wait('2');
     await I.see(LandingpageDetails.pageTitle);
     await I.see(LandingpageDetails.subHeading);
     await I.see(LandingpageDetails.hintMessage);
     await I.see(LandingpageDetails.descriptionL1);
     await I.see(LandingpageDetails.descriptionL2);
-    I.wait(5);
-    I.click('Start now');
+    await I.click(this.startButton);
   },
 };
