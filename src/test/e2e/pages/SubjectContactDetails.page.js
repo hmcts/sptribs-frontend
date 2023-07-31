d24cca6d8013d02995d1e156594930c8cb58b141
Jake Gowler <jakecgowler@gmail.com>
2023-07-31 14:43:55 +0100

    Subject contact details

---
+++ src/test/e2e/pages/SubjectContactDetails.page.js
@@ -0,0 +1,28 @@
+const { I } = inject();
+const subjectDetails = require('../fixtures/content/SubjectContactDetails_content');
+
+module.exports = {
+  fields: {
+    email: '#subjectEmailAddress',
+    mobileNumber: '#subjectContactNumber',
+  },
+
+  contactAgreeBox: '#subjectAgreeContact',
+  continueButton: '#main-form-submit',
+
+ checkPageLoads() {
+    I.waitForText(subjectDetails.pageTitle);
+    I.waitForText(subjectDetails.subHeading1);
+    I.waitForText(subjectDetails.subHeading2);
+    I.waitForText(subjectDetails.textOnPage1);
+    I.waitForText(subjectDetails.textOnPage2);
+    },
+
+  fillInfields() {
+    I.fillField(this.fields.email, subjectDetails.emailAddress);
+    I.fillField(this.fields.mobileNumber, subjectDetails.contactNumber);
+    I.click(this.contactAgreeBox);
+    I.click(this.continueButton);
+    I.waitForNavigation();
+  },
+};
