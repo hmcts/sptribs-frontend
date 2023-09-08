7a22a19f992588f62fbda97141a7380822da1da6
Jake Gowler <jakecgowler@gmail.com>
2023-09-08 12:09:04 +0100

    Cleanup

--- src/test/e2e/tests/smoke_test.js
+++ src/test/e2e/tests/smoke_test.js
@@ -1,16 +1,16 @@
 const config = require('../config');
 
-Feature('Smoke tests @smoke-tests @cross-browser ').retry(1);
+Feature('Smoke tests @smoke-tests @cross-browser').retry(1);
 
-Scenario('Send a get request to check the service is up.', async ({ loginPage, landingPage }) => {
+Scenario('Send a get request to check the service is up.', async () => {
   try {
-      const response = await fetch(config.baseUrl);
-      if (response.status === 200) {
-        console.log('Landing page is accessible. Smoke test passed.');
-      } else {
-        console.error('Landing page is not accessible. Smoke test failed. Status code:', response.status);
-      }
-    } catch (error) {
-      console.error('Failed to make the GET request:', error.message);
+    const response = await fetch(config.baseUrl);
+    if (response.status === 200) {
+      console.log('Landing page is accessible. Smoke test passed.');
+    } else {
+      console.error('Landing page is not accessible. Smoke test failed. Status code:', response.status);
     }
+  } catch (error) {
+    console.error('Failed to make the GET request:', error.message);
+  }
 });
