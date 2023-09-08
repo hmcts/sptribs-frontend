50a19926cd657edb8a08223f7833dd1403d07ca1
Jake Gowler <jakecgowler@gmail.com>
2023-09-08 12:57:55 +0100

    Get request change

--- src/test/tests/smoke_test.js
+++ src/test/tests/smoke_test.js
@@ -1,9 +1,16 @@
-const config = require('../e2e/config');
+const config = require('../config');
 
-Feature('Smoke tests @smoke-tests');
+Feature('Smoke tests @smoke-tests @cross-browser').retry(1);
 
-Scenario('Sign in as citizen and verify landing page', async ({ I, landingPage }) => {
-  await I.goToPage(config.baseUrl);
-  await I.signIn(config.citizenUserOne);
-  await landingPage.seeTheLandingPage();
+Scenario('Send a get request to check the service is up.', async () => {
+  try {
+    const response = await fetch(config.baseUrl);
+    if (response.status === 200) {
+      console.log('Landing page is accessible. Smoke test passed.');
+    } else {
+      console.error('Landing page is not accessible. Smoke test failed. Status code:', response.status);
+    }
+  } catch (error) {
+    console.error('Failed to make the GET request:', error.message);
+  }
 });
