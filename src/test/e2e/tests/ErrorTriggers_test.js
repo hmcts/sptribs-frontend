088ef088f344b472665c61344a49ab7c0ecb8f32
JakeCG <jake.gowler@HMCTS.net>
2023-10-04 11:17:30 +0100

    1189 reactivate tests (#451)
    * Reactivating tests
    
    * Back button update
    
    * Adding additional verification for error triggers journey

--- src/test/e2e/tests/ErrorTriggers_test.js
+++ src/test/e2e/tests/ErrorTriggers_test.js
@@ -2,39 +2,47 @@ const { I } = inject();
 
 Feature('Trigger errors @e2e-tests').retry(1);
 
-//Scenario(
-//  'Run through the entire application and check all error messaging',
-//  async ({
-//    landingPage,
-//    loginPage,
-//    subjectDetailsPage,
-//    subjectContactDetailsPage,
-//    representationPage,
-//    representationQualifiedPage,
-//    representativeDetailsPage,
-//    uploadAppealForm,
-//    uploadSupportingDocuments,
-//    uploadOtherInformation,
-//  }) => {
-//    await landingPage.seeTheLandingPage();
-//    await landingPage.continueOn();
-//    await loginPage.SignInUser();
-//    await subjectDetailsPage.triggerErrorMessages();
-//    await subjectDetailsPage.fillInFields();
-//    await subjectContactDetailsPage.triggerErrorMessages();
-//    await subjectContactDetailsPage.fillInFields();
-//    await representationPage.triggerErrorMessages();
-//    await representationPage.fillInFields();
-//    await representationQualifiedPage.triggerErrorMessages();
-//    await representationQualifiedPage.fillInFields();
-//    await representativeDetailsPage.triggerErrorMessages();
-//    await representativeDetailsPage.fillInFields();
-//    await uploadAppealForm.triggerErrorMessages();
-//    await uploadAppealForm.uploadDocumentsSection();
-//    await uploadSupportingDocuments.triggerErrorMessages();
-//    await uploadSupportingDocuments.uploadDocumentsSection();
-//    await uploadOtherInformation.triggerErrorMessages();
-//    await uploadOtherInformation.uploadDocumentsSection();
-//    await I.waitForSelector('button[name="opt-out-button"]'); // Just checks that it's got to this page
-//  }
-//);
+Scenario(
+  'Run through the entire application and check all error messaging',
+  async ({
+    landingPage,
+    loginPage,
+    subjectDetailsPage,
+    subjectContactDetailsPage,
+    representationPage,
+    representationQualifiedPage,
+    representativeDetailsPage,
+    uploadAppealForm,
+    uploadSupportingDocuments,
+    uploadOtherInformation,
+  }) => {
+    await landingPage.seeTheLandingPage();
+    await landingPage.continueOn();
+    await loginPage.SignInUser();
+    await subjectDetailsPage.checkPageLoads(false);
+    await subjectDetailsPage.triggerErrorMessages();
+    await subjectDetailsPage.fillInFields();
+    await subjectContactDetailsPage.checkPageLoads(false);
+    await subjectContactDetailsPage.triggerErrorMessages();
+    await subjectContactDetailsPage.fillInFields();
+    await representationPage.checkPageLoads(false);
+    await representationPage.triggerErrorMessages();
+    await representationPage.fillInFields();
+    await representationQualifiedPage.checkPageLoads(false);
+    await representationQualifiedPage.triggerErrorMessages();
+    await representationQualifiedPage.fillInFields();
+    await representativeDetailsPage.checkPageLoads(false);
+    await representativeDetailsPage.triggerErrorMessages();
+    await representativeDetailsPage.fillInFields();
+    await uploadAppealForm.checkPageLoads(false);
+    await uploadAppealForm.triggerErrorMessages();
+    await uploadAppealForm.uploadDocumentsSection();
+    await uploadSupportingDocuments.checkPageLoads(false);
+    await uploadSupportingDocuments.triggerErrorMessages();
+    await uploadSupportingDocuments.uploadDocumentsSection();
+    await uploadOtherInformation.checkPageLoads(false);
+    await uploadOtherInformation.triggerErrorMessages();
+    await uploadOtherInformation.uploadDocumentsSection();
+    await I.waitForSelector('button[name="opt-out-button"]'); // Just checks that it's got to this page
+  }
+);
