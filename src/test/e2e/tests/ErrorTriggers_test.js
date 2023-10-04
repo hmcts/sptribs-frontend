2118988e201146d05cfbe447c8bb24d8909498df
Jake Gowler <jakecgowler@gmail.com>
2023-10-04 10:36:19 +0100

    Adding additional verification for error triggers journey

--- src/test/e2e/tests/ErrorTriggers_test.js
+++ src/test/e2e/tests/ErrorTriggers_test.js
@@ -19,20 +19,28 @@ Scenario(
     await landingPage.seeTheLandingPage();
     await landingPage.continueOn();
     await loginPage.SignInUser();
+    await subjectDetailsPage.checkPageLoads(false);
     await subjectDetailsPage.triggerErrorMessages();
     await subjectDetailsPage.fillInFields();
+    await subjectContactDetailsPage.checkPageLoads(false);
     await subjectContactDetailsPage.triggerErrorMessages();
     await subjectContactDetailsPage.fillInFields();
+    await representationPage.checkPageLoads(false);
     await representationPage.triggerErrorMessages();
     await representationPage.fillInFields();
+    await representationQualifiedPage.checkPageLoads(false);
     await representationQualifiedPage.triggerErrorMessages();
     await representationQualifiedPage.fillInFields();
+    await representativeDetailsPage.checkPageLoads(false);
     await representativeDetailsPage.triggerErrorMessages();
     await representativeDetailsPage.fillInFields();
+    await uploadAppealForm.checkPageLoads(false);
     await uploadAppealForm.triggerErrorMessages();
     await uploadAppealForm.uploadDocumentsSection();
+    await uploadSupportingDocuments.checkPageLoads(false);
     await uploadSupportingDocuments.triggerErrorMessages();
     await uploadSupportingDocuments.uploadDocumentsSection();
+    await uploadOtherInformation.checkPageLoads(false);
     await uploadOtherInformation.triggerErrorMessages();
     await uploadOtherInformation.uploadDocumentsSection();
     await I.waitForSelector('button[name="opt-out-button"]'); // Just checks that it's got to this page
