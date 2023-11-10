3733627aaf54846df4ab845adf61c228c2e5baa1
gabriellemagruder <gabrielle.magruder@solirius.com>
2023-11-10 15:50:39 +0000

    Upgrade connect-redis from v6.0.0 to v7.1.0 and redis from v3.1.2 to v4.6.10. Remove types/redis and types/connect-redis.

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
   setupFilesAfterEnv: ['jest-extended'],
   moduleNameMapper: {
     '@hmcts/nodejs-logging': '<rootDir>/src/test/unit/mocks/hmcts/nodejs-logging',
+    'connect-redis': '<rootDir>/node_modules/connect-redis/dist/cjs'
   },
   coverageThreshold: {
     global: {
