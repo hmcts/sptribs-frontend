5fe9024ea94eb50869f7991414f791a7cdb94afc
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2023-11-17 10:51:04 +0000

    DTSSTCI-52: Fix label content for Welsh button on landing page

--- src/main/steps/home/get.ts
+++ src/main/steps/home/get.ts
@@ -1,32 +1,15 @@
 import { Response } from 'express';
 
-// import { Case } from '../../app/case/case';
-// import { State } from '../../app/case/definition';
 import { AppRequest } from '../../app/controller/AppRequest';
-// import { Form, FormFields } from '../../app/form/Form';
-// import { form as applicant1FirstQuestionForm } from '../applicant1/applying-with/content';
-import {
-  //USER_ROLE,
-  //CITIZEN_HOME_URL,
-  SUBJECT_DETAILS,
-} from '../urls';
+import { SUBJECT_DETAILS } from '../urls';
 
 export class HomeGetController {
   public get(req: AppRequest, res: Response): void {
-    // if (req.session.userCase.divorceOrDissolution !== res.locals.serviceType) {
-    //   throw new Error('Invalid case type');
-    // }
-    // const firstQuestionForm = getApplicantFirstQuestionForm();
     const isFirstQuestionComplete = true;
-
-    res.redirect(applicant1RedirectPageSwitch(isFirstQuestionComplete));
+    res.redirect(pageRedirectSwitch(isFirstQuestionComplete));
   }
 }
 
-const applicant1RedirectPageSwitch = (isFirstQuestionComplete: boolean) => {
+const pageRedirectSwitch = (isFirstQuestionComplete: boolean) => {
   return isFirstQuestionComplete ? SUBJECT_DETAILS : SUBJECT_DETAILS;
 };
-
-// const getApplicantFirstQuestionForm = () => {
-//   return new Form(<FormFields>applicant1FirstQuestionForm.fields);
-// };
