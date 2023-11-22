dd47893b7f4a677d9de3b2502ab8c6dc1c265d85
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2023-11-22 09:35:29 +0000

    DTSSTCI-36: Add welsh translations provided by translation team

--- src/main/steps/common/common.content.ts
+++ src/main/steps/common/common.content.ts
@@ -115,7 +115,7 @@ const cy: typeof en = {
   applyForAdoption: 'Apply for a service" (in welsh)',
   applyForDissolution: 'Apply for a service"(in welsh)',
   feedback:
-    'This is a new service – your <a class="govuk-link" aria-label="Feedback link, This will open a new tab. You’ll need to return to this tab and continue with your application within 60 mins so you don’t lose your progress." href="currentUrl" target="_blank">feedback</a> will help us to improve it.(in welsh)',
+    "<b>Helpwch ni i wella’r gwasanaeth hwn</b><br> Mae hwn yn wasanaeth newydd. Helpwch ni ei wella i bobl eraill trwy <a class='govuk-link' href='https://www.smartsurvey.co.uk/s/Specials_Feedback/?pageurl=currentUrl' target=_blank>roi eich adborth</a>",
   languageToggle: '<a href="?lng=en" class="govuk-link language">English</a>',
   govUk: 'GOV.UK',
   back: 'Back (in welsh)',
