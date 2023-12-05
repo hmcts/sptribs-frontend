17f346d4af3019d3c837dbc9418690183a941320
gabriellemagruder <gabrielle.magruder@solirius.com>
2023-12-05 09:42:20 +0000

    Revert "Remove jest-circus as not used in application"
    This reverts commit c1828598417ec538388bb0a2eac052f3309d2478.

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testRegex: '(/src/test/.*|\\.test)\\.(ts|js)$',
   moduleFileExtensions: ['ts', 'js'],
   testEnvironment: 'node',
+  testRunner: 'jest-circus/runner',
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
