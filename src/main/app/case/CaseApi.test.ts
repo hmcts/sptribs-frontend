e891a87506a64cbf6cac76231f1eda360089dfa0
Abdullah Shah <abdullah-shah97@hotmail.com>
2023-12-11 14:52:22 +0000

    Reformatted

--- src/main/app/case/CaseApi.test.ts
+++ src/main/app/case/CaseApi.test.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { LoggerInstance } from 'winston';
 
 import { UserDetails } from '../controller/AppRequest';
+
 import { CaseApi } from './CaseApi';
 
 const { Logger } = require('@hmcts/nodejs-logging');
@@ -14,9 +15,9 @@ test('Should return case roles for userId and caseId passed', async () => {
     data: {
       case_users: [
         {
-          case_id: '1624351572550045',
-          user_id: '372ff9c1-9930-46d9-8bd2-88dd26ba2475',
-          case_role: '[APPLICANTTWO]',
+        case_id: '1624351572550045',
+        user_id: '372ff9c1-9930-46d9-8bd2-88dd26ba2475',
+        case_role: '[APPLICANTTWO]',
         },
       ],
     },
@@ -26,7 +27,7 @@ test('Should return case roles for userId and caseId passed', async () => {
     id: '372ff9c1-9930-46d9-8bd2-88dd26ba2475',
     email: 'harry@hog.com',
     givenName: 'harry',
-    familyName: 'potter'
+    familyName: 'potter',
   };
 
   const case_id = '1624351572550045';
@@ -34,14 +35,14 @@ test('Should return case roles for userId and caseId passed', async () => {
   const result = await caseApiInstance.getCaseUserRoles(case_id, userDetails.id);
   expect(result).toEqual(
     {
-      case_users: [
-        {
-          case_id: '1624351572550045',
-          user_id: '372ff9c1-9930-46d9-8bd2-88dd26ba2475',
-          case_role: '[APPLICANTTWO]',
-        }]
+    case_users: [
+      {
+        case_id: '1624351572550045',
+        user_id: '372ff9c1-9930-46d9-8bd2-88dd26ba2475',
+        case_role: '[APPLICANTTWO]',
+      }]
     });
-  expect(mockedAxios.get).toHaveBeenCalledWith('case-users?case_ids=1624351572550045&user_ids=372ff9c1-9930-46d9-8bd2-88dd26ba2475');
+expect(mockedAxios.get).toHaveBeenCalledWith('case-users?case_ids=1624351572550045&user_ids=372ff9c1-9930-46d9-8bd2-88dd26ba2475');
 });
 
 test('Should throw error when case roles could not be fetched', async () => {
