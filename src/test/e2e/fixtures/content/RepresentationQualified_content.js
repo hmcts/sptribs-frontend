7d83bfc9f5b4d4abc6b203133638c10ea9543e25
JakeCG <jake.gowler@HMCTS.net>
2024-01-25 12:46:43 +0000

    Removing E2E tests (#589)
    * Removing E2E tests
    
    * DTSSTCI-349: Bumped up follow-redirects package (#590)
    
    ---------
    
    Co-authored-by: Rajaram <70951210+rajaram-nagarajan@users.noreply.github.com>

--- src/test/e2e/fixtures/content/RepresentationQualified_content.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    pageTitle:'Is the named representative legally qualified?',
-    hintMessage: 'A solicitor or a barrister are legally qualified representatives.',
-    textOnPage1:'Yes',
-    textOnPage2:'No',
-    button:'Continue',
-    errorBanner:'There is a problem',
-    selectionError: 'Please select an answer before you can proceed further',
- };
--- src/test/e2e/fixtures/content/Representation_content.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    pageTitle:'Is there a representative named on completed tribunal form?',
-    textOnPage1:'Yes',
-    textOnPage2:'No',
-    button:'Continue',
-    errorBanner:'There is a problem',
-    selectionError: 'Please select an answer before you can proceed further',
- };
