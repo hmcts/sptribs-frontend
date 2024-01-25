f3ea31fa7f37f40c60257c39a78aa874bea65ef2
JakeCG <jake.gowler@HMCTS.net>
2024-01-25 12:46:43 +0000

    Removing E2E tests (#589)
    * Removing E2E tests
    
    * DTSSTCI-349: Bumped up follow-redirects package (#590)
    
    ---------
    
    Co-authored-by: Rajaram <70951210+rajaram-nagarajan@users.noreply.github.com>

--- src/test/e2e/fixtures/content/LandingPage_content.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    pageTitle: 'Submit a First-tier Tribunal form',
-    hintMessage: 'Use this service to upload and submit a completed First-tier Tribunal form. You\'ll also be asked to upload any supporting documents separately, so make sure you have everything you need to hand before getting started. There will be no option to save your progress and return later once you begin.',
-    subHeading: 'Submitting your form',
-    descriptionL1: 'As well as uploading a tribunal form, you will also be asked a handful of simple questions. You\'ll be able to review and change the answers you give to these questions before you submit them.',
-    descriptionL2: 'Uploading and submitting a tribunal form should not take longer than 20 minutes.',
-   };
-
