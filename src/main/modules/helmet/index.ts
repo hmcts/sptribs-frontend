cd128c667973a3e719bfbaa44aa1873eaa617702
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-03-07 10:01:19 +0000

    DTSSTCI-557: Update connectSrc

--- src/main/modules/helmet/index.ts
+++ src/main/modules/helmet/index.ts
@@ -42,7 +42,7 @@ export class Helmet {
     app.use(
       helmet.contentSecurityPolicy({
         directives: {
-          connectSrc: [self],
+          connectSrc: [self, dynatraceDomain],
           defaultSrc: ["'none'"],
           fontSrc: [self, 'data:'],
           imgSrc: [self, googleAnalyticsDomain],
