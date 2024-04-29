4735113f9ef3b88f6c70f5746a14aa453ca2b2c4
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-04-29 15:17:23 +0100

    DTSSTCI-754: Update express file upload config so that files are not trimmed to 100MB - instead 101MB so error handler picks up oversized files

--- src/main/modules/fileupload/index.ts
+++ src/main/modules/fileupload/index.ts
@@ -3,7 +3,7 @@ import fileUpload from 'express-fileupload';
 
 export class FileUpload {
   public enableFor(app: Application): void {
-    const DefaultFileUploadSize: number = 100;
+    const DefaultFileUploadSize: number = 101;
     app.use(
       fileUpload({
         limits: { fileSize: 1024 * 1024 * DefaultFileUploadSize },
