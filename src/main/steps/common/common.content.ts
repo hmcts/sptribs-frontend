26c4e8d9c3194293783edacc463e4b3688c7df9c
Phoebe Revolta <phoebe.revolta@solirius.com>
2024-06-20 11:23:59 +0100

    DTSSTCI-730: revert another change that is out of scope of ticket as errorSendingInvite is not used

--- src/main/steps/common/common.content.ts
+++ src/main/steps/common/common.content.ts
@@ -23,9 +23,9 @@ const en = {
   warning: 'Warning',
   required: 'You have not answered the question. You need to select an answer before continuing.',
   notAnswered: 'You have not answered the question.',
-  errorSaving: 'We’re having technical problems saving your application. Try again in a few minutes.',
+  errorSaving: 'Sorry, we’re having technical problems saving your application. Please try again in a few minutes.',
   errorSendingInvite:
-    'We’re having technical problems sending your application for review. Try again in a few minutes.',
+    'Sorry, we’re having technical problems sending your application for review. Please try again in a few minutes.',
   ogl: 'All content is available under the <a class="govuk-footer__link" href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence v3.0</a>, except where otherwise stated',
   copyright: '© Crown copyright',
   errorSummaryHeading: 'There is a problem',
