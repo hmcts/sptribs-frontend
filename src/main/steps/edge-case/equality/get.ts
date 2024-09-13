2cd7e1c9d7c74e6c17f01eee09150b383258174b
Tom Elliott <tomlaelliott@gmail.com>
2024-09-13 11:19:13 +0100

    Create change to force redeployment of fe pods (#690)
    Co-authored-by: Tom Elliott <tomelliott@Toms-MacBook-Pro.local>

--- src/main/steps/edge-case/equality/get.ts
+++ src/main/steps/edge-case/equality/get.ts
@@ -18,7 +18,6 @@ export default class PCQGetController {
   public async get(req: AppRequest, res: Response): Promise<void> {
     const pcqUrl: string = config.get('services.equalityAndDiversity.url');
     const pcqEnabled: boolean = String(config.get('services.equalityAndDiversity.enabled')) === 'true';
-
     if (!pcqEnabled) {
       return res.redirect(CHECK_YOUR_ANSWERS);
     }
