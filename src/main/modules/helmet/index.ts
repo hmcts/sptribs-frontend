20f623786e533a19336f2d3d9301263c7519bb6a
Phoebe Revolta <phoebe.revolta@solirius.com>
2025-07-28 14:28:58 +0100

    DTSSTCI-1313: break up address & add content policies

--- src/main/modules/helmet/index.ts
+++ src/main/modules/helmet/index.ts
@@ -9,6 +9,12 @@ export interface HelmetConfig {
 const dynatraceDomain = 'https://*.dynatrace.com';
 const googleAnalyticsDomain = 'https://*.google-analytics.com';
 const googleTagManagerDomain = 'https://*.googletagmanager.com';
+const webchatAssets = 'https://vcc-assets.8x8.com/';
+const webchatDomain1 = 'https://cloud8-cc-geo.8x8.com/vcc-chat-channels/public/webchat/';
+const webchatDomain2 = 'https://cloud8-uk-cf.8x8.com/';
+const webchatScriptsDomain = 'https://vcc-eu4-cf.8x8.com/';
+const webchatAPI1 = 'https://api.global.chalet.8x8.com/';
+const webchatAPI2 = 'wss://api.uk.chalet.8x8.com/';
 const self = "'self'";
 
 /**
@@ -31,8 +37,14 @@ export class Helmet {
       googleAnalyticsDomain,
       googleTagManagerDomain,
       dynatraceDomain,
+      webchatAssets,
+      webchatDomain1,
+      webchatDomain2,
+      webchatScriptsDomain,
       "'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw='",
       "'sha256-XbrIe2Mu+2yK4boWprqdknTXJvaHzNkq5hBOh5NMUwE='",
+      "'sha256-fp5pXYMr04xi3uGQWSSNcP/yvslgQKHo5fak98t2zbs='",
+      "'sha256-iIZgFWxwwTfXESfktlUZ3OqwQ8kLwWS1sKcO4fGGKcM='",
     ];
 
     if (app.locals.developmentMode) {
@@ -42,13 +54,23 @@ export class Helmet {
     app.use(
       helmet.contentSecurityPolicy({
         directives: {
-          connectSrc: [self, dynatraceDomain],
+          connectSrc: [
+            self,
+            dynatraceDomain,
+            webchatAssets,
+            webchatDomain1,
+            webchatDomain2,
+            webchatScriptsDomain,
+            webchatAPI1,
+            webchatAPI2,
+          ],
           defaultSrc: ["'none'"],
           fontSrc: [self, 'data:'],
-          imgSrc: [self, googleAnalyticsDomain],
+          imgSrc: [self, googleAnalyticsDomain, googleTagManagerDomain, webchatScriptsDomain],
           objectSrc: [self],
           scriptSrc,
-          styleSrc: [self],
+          styleSrc: [self, "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='"],
+          frameSrc: ['blob:'],
         },
       }) as RequestHandler
     );
