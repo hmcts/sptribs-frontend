c20ad39f3b182a438e3a10536b489fee64e42304
Phoebe Revolta <phoebe.revolta@solirius.com>
2025-08-19 18:48:59 +0100

    DTSSTCI-1366: add test for from-api-format cicaReferenceNumber conversion

--- src/main/app/case/from-api-format.test.ts
+++ src/main/app/case/from-api-format.test.ts
@@ -1,4 +1,4 @@
-import { CaseData } from './definition';
+import { CaseData, EditCicaCaseDetails } from './definition';
 import { fromApiFormat } from './from-api-format';
 
 describe('from-api-format', () => {
@@ -20,6 +20,17 @@ describe('from-api-format', () => {
       } as unknown as CaseData);
 
       expect(nfdivFormat).toMatchObject({});
+    });
+  });
+
+  describe('pulling CICA reference number from editCicaCaseDetails to cicaReferenceNumber', () => {
+    test('extracts to cicaReferenceNumber', () => {
+      const nfdivFormat = fromApiFormat({
+        ...results,
+        editCicaCaseDetails: { cicaReferenceNumber: 'CICA123456' } as EditCicaCaseDetails,
+      } as unknown as CaseData);
+
+      expect(nfdivFormat).toStrictEqual({ cicaReferenceNumber: 'CICA123456' });
     });
   });
 });
