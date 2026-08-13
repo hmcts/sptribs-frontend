a712c582a7fa7655392c7b6a3febddf414ffd167
P Revolta <phoebe.revolta@solirius.com>
2026-08-13 10:55:32 +0100

    DTSSTCI-1831: replace case/appeal number with HMCTS number

--- src/main/steps/edge-case/application-submitted/content.ts
+++ src/main/steps/edge-case/application-submitted/content.ts
@@ -14,7 +14,7 @@ export const generateContent: TranslationFn = content => {
     const caseId = userCase?.id?.toString().replace(/.{4}/g, '$& - ').substring(0, 25);
     return {
       ...Translations.en,
-      referenceNumber: `<strong>Appeal Number:</font><br>${caseId}</strong>`,
+      referenceNumber: `<strong>HMCTS Number:</font><br>${caseId}</strong>`,
       line2: 'An email will be sent to ' + subjectEmail + ', that explains what happens next.',
       line3:
         "<b>We would like to hear your thoughts</b><br>Complete this short 5-minutes survey to help improve our services for you and others <a class='govuk-link' href='https://www.smartsurvey.co.uk/s/SurveyExit/?service=Specials'target=_blank><br>Please leave your your feedback</a>",
