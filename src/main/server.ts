79e9bceaa1dc10788ab5368aa57e7d99416443b1
Mihai Tranca <mihai.tranca@justice.gov.uk>
2026-09-02 08:37:10 +0100

    Load Key Vault properties before any config.get() call makes config immutable.

--- src/main/server.ts
+++ src/main/server.ts
@@ -65,10 +65,9 @@ app.use((req, res, next) => {
 });
 app.disable('x-powered-by');
 app.disable('X-Powered-By');
-new FileUpload().enableFor(app);
-
-new AxiosLogger().enableFor(app);
 new PropertiesVolume().enableFor(app);
+new FileUpload().enableFor(app);
+new AxiosLogger().enableFor(app);
 new ErrorHandler().enableFor(app, logger);
 new LoadTimeouts().enableFor(app);
 new Nunjucks().enableFor(app);
